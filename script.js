adaa7bafa7675b3b53d7c288ce233846b7d556a3
William Yu <88666309+wyu6609@users.noreply.github.com>
2022-02-15 10:17:23 -0500

    added geolocation api, started search bar fxn

--- script.js
+++ script.js
@@ -1,31 +1,59 @@
-const uri = "https://data.cityofnewyork.us/resource/43nn-pn8j.json";
+const uri = "https://data.cityofnewyork.us/resource/43nn-pn8j.json?dba=";
 //fetch api
-fetch(uri, {
-  method: "GET",
-  headers: {
-    "Content-Type": "application/json",
+function fetchRestaurant(restaurantName) {
+  fetch(`${uri}${restaurantName}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Success:", data);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+//geolocation
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    let y = position.coords.latitude;
+    let x = position.coords.longitude;
+    console.log(position);
+    console.log(y, x);
+    // initMap(position);
+    initMap(y, x);
+    //use geolocation coords when available
   },
-})
-  .then((response) => response.json())
-  .then((data) => {
-    console.log("Success:", data);
-  })
-  .catch((error) => {
-    console.error("Error:", error);
-  });
+  function () {
+    alert("Browser Location denied!");
+    initMap(40.70521, -74.013757);
+  }
+);
+// if ("geolocation" in navigator) {
+//   navigator.geolocation.getCurrentPosition((position) => {
+//     let y = position.coords.latitude;
+//     let x = position.coords.longitude;
+//     console.log(position);
+//     console.log(y, x);
+//     // initMap(position);
+//     initMap(y, x);
+//     //use geolocation coords when available
+//   });
+// } else {
+//   alert("please enable browser location!");
+//   //use FLAT IRON SCHOOL coordinates as default if geolocation not available
+// }
 
-//google maps intial load
-function initMap() {
+//google maps initial load
+function initMap(y, x) {
   //Map options
+  console.log(y, x);
   let options = {
     zoom: 15,
-    center: { lat: 40.7053, lng: -74.0139 },
+    center: { lat: y, lng: x },
   };
   //new map
   let map = new google.maps.Map(document.getElementById("map"), options);
   //map options
   let marker = new google.maps.Marker({
-    position: { lat: 40.7053, lng: -74.0139 },
+    position: { lat: y, lng: x },
     draggable: true,
     animation: google.maps.Animation.DROP,
     map: map,
@@ -53,3 +81,39 @@ function addMarker(coords) {
     map: map,
   });
 }
+//////////////////////////////////////////////////////////////////////////////////////////////
+//search bar -->
+
+function searchRestaurant() {
+  console.log(document.querySelector("#search-bar").value.toUpperCase());
+  // fetchRestaurant(document.querySelector("#search-bar").value.toUpperCase());
+}
+
+document.querySelector("#search-button").addEventListener("click", function () {
+  console.log("clicked");
+  searchRestaurant();
+});
+
+document
+  .querySelector("#search-bar")
+  .addEventListener("keyup", function (event) {
+    console.log(document.querySelector("#search-bar").value.toUpperCase());
+    if (event.key == "Enter") {
+      searchRestaurant();
+    }
+  });
+// const charactersList = document.getElementById("charactersList");
+// const searchBar = document.getElementById("searchBar");
+// let hpCharacters = [];
+
+// searchBar.addEventListener("keyup", (e) => {
+//   const searchString = e.target.value.toLowerCase();
+//   const filterCharacters = hpCharacters.filter((character) => {
+//     return (
+//       character.name.toLowerCase().includes(searchString) ||
+//       character.house.toLowerCase().includes
+//     );
+//   });
+// });
+//////////////////////////////////////////////////////////////////////////////////////////////
+
