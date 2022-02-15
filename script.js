f141ef64623230ce8e6fb620d8b203a604b47d86
William Yu <wy427@nyu.edu>
2022-02-14 21:07:45 -0500

    added google Maps, with some drop flag animation

--- script.js
+++ script.js
@@ -13,3 +13,29 @@ fetch(uri, {
   .catch((error) => {
     console.error("Error:", error);
   });
+
+//google maps
+function initMap() {
+  //Map options
+  let options = {
+    zoom: 15,
+    center: { lat: 40.7053, lng: -74.0139 },
+  };
+  //new map
+  let map = new google.maps.Map(document.getElementById("map"), options);
+  //map options
+  let marker = new google.maps.Marker({
+    position: { lat: 40.7053, lng: -74.0139 },
+    draggable: true,
+    animation: google.maps.Animation.DROP,
+    map: map,
+    icon: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
+  });
+  function toggleBounce() {
+    if (marker.getAnimation() !== null) {
+      marker.setAnimation(null);
+    } else {
+      marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
+  }
+}
