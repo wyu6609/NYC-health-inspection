6c297726c1133330a948c7685658b5ae0c6ac284
William Yu <88666309+wyu6609@users.noreply.github.com>
2022-02-15 16:38:02 -0500

    added search bar functionality

--- script.js
+++ script.js
@@ -1,12 +1,72 @@
 // api endpoint
 const uri = "https://data.cityofnewyork.us/resource/43nn-pn8j.json?dba=";
+let map;
+
+//add markers for selected restaurant
+function addMarker(coords, obj) {
+  let marker = new google.maps.Marker({
+    position: coords,
+    map: map,
+    draggable: true,
+    animation: google.maps.Animation.DROP,
+    title: obj.grade,
+  });
+  function toggleBounce() {
+    if (marker.getAnimation() !== null) {
+      marker.setAnimation(null);
+    } else {
+      marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
+  }
+  const contentString = `
+  <h2>${obj.dba}</h2>
+  <h3>Grade: ${obj.grade}</h3>
+  <h3>Score: ${obj.score}</h3>
+  <p>An inspection score of 0 to 13 is an A, 14 to 27 points is a B, and 28 or more points is a C.<p>`;
+  const infowindow = new google.maps.InfoWindow({
+    content: contentString,
+  });
+
+  marker.addListener("click", () => {
+    console.log(obj);
+    // create div on selected marker
+    infowindow.open({
+      anchor: marker,
+      map,
+      shouldFocus: false,
+    });
+  });
+}
+
+//fetch api
+function fetchRestaurant(restaurantName) {
+  fetch(`${uri}${restaurantName}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Success:");
+      data.forEach((obj) => {
+        let coords = {
+          lat: parseFloat(obj.latitude),
+          lng: parseFloat(obj.longitude),
+        };
+
+        console.log(obj);
+        addMarker(coords, obj);
+      });
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
 //geolocation
 navigator.geolocation.getCurrentPosition(
   function (position) {
     let y = position.coords.latitude;
     let x = position.coords.longitude;
-    console.log(position);
-    console.log(y, x);
+
+    // console.log(position);
+    // console.log(y, x);
     // initMap(position);
     initMap(y, x);
     //use geolocation coords when available
@@ -16,28 +76,16 @@ navigator.geolocation.getCurrentPosition(
     initMap(40.70521, -74.013757);
   }
 );
-//fetch api
-function fetchRestaurant(restaurantName) {
-  fetch(`${uri}${restaurantName}`)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("Success:", data);
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
-}
-
 //google maps initial load
 function initMap(y, x) {
   //Map options
   console.log(y, x);
   let options = {
-    zoom: 15,
+    zoom: 14,
     center: { lat: y, lng: x },
   };
   //new map
-  let map = new google.maps.Map(document.getElementById("map"), options);
+  map = new google.maps.Map(document.getElementById("map"), options);
   //map options
   let marker = new google.maps.Marker({
     position: { lat: y, lng: x },
@@ -55,25 +103,12 @@ function initMap(y, x) {
   }
 }
 
-let markers = []; //insert search query coords and content
-//loop through markers
-for (let i = 0; i < markers.length; i++) {
-  addMarker(markers[i]);
-}
-
-//add markers for selected restaurant
-function addMarker(coords) {
-  let marker = new google.maps.Marker({
-    position: coords,
-    map: map,
-  });
-}
 //////////////////////////////////////////////////////////////////////////////////////////////
 //search bar -->
 
 function searchRestaurant() {
   console.log(document.querySelector("#search-bar").value.toUpperCase());
-  // fetchRestaurant(document.querySelector("#search-bar").value.toUpperCase());
+  fetchRestaurant(document.querySelector("#search-bar").value.toUpperCase());
 }
 
 document.querySelector("#search-button").addEventListener("click", function () {
@@ -89,6 +124,13 @@ document
       searchRestaurant();
     }
   });
+
+function delMarkers(map) {
+  map.clearOverlays();
+}
+document.querySelector("#remove-markers-btn").addEventListener("click", () => {
+  console.log("clicked");
+});
 // const charactersList = document.getElementById("charactersList");
 // const searchBar = document.getElementById("searchBar");
 // let hpCharacters = [];
